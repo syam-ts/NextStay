19300ad7e062c286df2b4630c84ca1cca53b2603
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-09-01 22:33:37 +0530

    *

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBed , faPlane , faCar , faTaxi , faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { faBed , faPlane , faCar , faTaxi , faCalendarDays, faPerson } from "@fortawesome/free-solid-svg-icons";
+import { DateRange } from 'react-date-range'
 
 export default function Header() {
+
+const [date, setDate] = useState([
+    {
+        startDate: new Date(),
+        endDate: null,
+        key: 'selection'
+    }
+])
+
   return (
     <>
       <div className="bg-blue-950 text-white flex pl-44 gap-16 h-20">
@@ -40,16 +50,20 @@ export default function Header() {
         <div className="pl-44 text-white pt-5 pb-5">
           <button className="bg-sky-600 p-1">Sign in / Register</button>
         </div>
-        <div className="flex gap-20 ml-44 bg-white mr-96 rounded-sm border border-yellow-400"> 
+        <div className="flex p-1 gap-20 ml-44 bg-white mr-96 rounded-sm border border-yellow-400"> 
             <FontAwesomeIcon className="" icon={faBed} />
-            <input type="text" className="border border-gray-950" placeholder="Where are you going" /> 
-          <div className="border border-gray-900">
+            <input type="text" className="cursor-pointer" placeholder="Where are you going" /> 
+          <div >
             <FontAwesomeIcon className="" icon={faCalendarDays} />
-           <span className=" pl-3">date to date</span>
+           <span className="cursor-pointer pl-3">date to date</span>
           </div>
-          <div className="border border-gray-900">
-            <FontAwesomeIcon className=" " icon={faBed} />
-            <span className="pl-3">2 adult 2 chldren 1 room</span>
+          
+          <div>
+            <FontAwesomeIcon className=" " icon={faPerson} />
+            <span className="pl-3 cursor-pointer">2 adult 2 chldren 1 room</span>
+          </div>
+          <div className="ml-60">
+                  <button className="bg-sky-600 p-1 text-white border border-white">Search</button>
           </div>
         </div>
       </div>
