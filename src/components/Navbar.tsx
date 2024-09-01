16c84b3d2040d133277f376be6d1b9d71d6d796f
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-09-01 14:24:37 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+
 
 function Navbar() {
   return (
-    <div>
-        <div>
-            <span className="logo">
-                nextStay
-            </span>
-            <div>
-                <button> Register </button>
-                <button> Login </button>
-            </div>
-        </div>
+    <div className="bg-blue-900 flex h-20 justify-between">
+      <div className="text-white">
+        <span> NextStay </span>
+      </div>
+      <div className="flex">
+        <button className="bg-white text-gray-700 m-4"> Register </button>
+        <button className="bg-white text-gray-700 m-4"> Login </button>
+      </div>
     </div>
   )
 }
 
+
+
 export default Navbar
+
